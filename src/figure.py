d3174a33006bae4de122e029ee76699a4aa87896
Anton Kazancev <anton.a.kazancev@gmail.com>
2024-01-28 16:28:32 +0300

    minor fix

--- src/figure.py
+++ src/figure.py
@@ -21,3 +21,25 @@ class Figure(ABC):
     @abstractmethod
     def shift(self, x_shift: int, y_shift: int) -> "Figure":
         pass
+
+
+class Triangle(Figure):
+    def __init__(self, dots: ((int, int), (int, int), (int, int)), color: Color) -> None:
+        self.dots = tuple(dots)
+        self.color = color
+
+    def dots(self) -> list[(int, int)]:
+        return list(self.dots)
+
+    def color(self) -> Color:
+        return self.color
+
+    def command(self) -> str:
+        result = "draw triangle"
+        for dot in self.dots:
+            result += f" {dot[0]} {dot[1]}"
+        return result
+
+    def shift(self, x_shift: int, y_shift: int) -> Figure:
+        new_dots = [(dot[0] + x_shift, dot[1] + y_shift) for dot in self.dots]
+        return Triangle(new_dots, self.color)
--- src/triangulate.py
+++ src/triangulate.py
@@ -3,31 +3,9 @@ from figure import Figure
 from color import Color
 
 
-class Triangle(Figure):
-    def __init__(self, dots: ((int, int), (int, int), (int, int)), color: Color) -> None:
-        self.dots = tuple(dots)
-        self.color = color
-
-    def dots(self) -> list[(int, int)]:
-        return list(self.dots)
-
-    def color(self) -> Color:
-        return self.color
-
-    def command(self) -> str:
-        result = "draw triangle"
-        for dot in self.dots:
-            result += f" {dot[0]} {dot[1]}"
-        return result
-
-    def shift(self, x_shift: int, y_shift: int) -> Figure:
-        new_dots = [(dot[0] + x_shift, dot[1] + y_shift) for dot in self.dots]
-        return Triangle(new_dots, self.color)
-
-
-def triangulate_from_file(path: str) -> list[Triangle]:
+def triangulate_from_file(path: str) -> list[Figure]:
     return triangulate(Image.open(path))
 
 
-def triangulate(gif: Image) -> list[Triangle]:
+def triangulate(gif: Image) -> list[Figure]:
     return 0
