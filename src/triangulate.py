7cf51a4011a4e8f13c4ef3b42381f402b999451a
Anton Kazancev <anton.a.kazancev@gmail.com>
2024-01-28 17:56:17 +0300

    better typing

--- src/triangulate.py
+++ src/triangulate.py
@@ -1,10 +1,11 @@
 from PIL import Image
 from figure import Figure, Triangle
+from utils import *
 
 
-def triangulate_from_file(path: str) -> list[Figure]:
+def triangulate_from_file(path: str) -> list[Picture]:
     return triangulate(Image.open(path))
 
 
-def triangulate(gif: Image) -> list[Figure]:
+def triangulate(gif: Image) -> list[Picture]:
     pass
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,5 @@
+from figure import Figure
+
 Dot = tuple[int, int]
 Color = tuple[int, int, int]
+Picture = list[Figure]
