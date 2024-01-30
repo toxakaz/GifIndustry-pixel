61acf8ae2f729ca4eaa86bb033c92e605cb1d00e
Anton Kazancev <anton.a.kazancev@gmail.com>
2024-01-30 21:23:30 +0300

    clustering and rudimentary drawing
    first attempt, pls tst >.<

--- src/main.py
+++ src/main.py
@@ -1,1 +1,41 @@
-print("oh hello")
+from PIL import Image, ImageDraw
+import numpy as np
+from triangulate import clustering, build_triangles_delaunay
+from sklearn.preprocessing import MinMaxScaler
+
+img = Image.open("../data/imgs/parrots.webp")
+img.convert("RGB")
+
+img_array = np.array(img)
+pixels = []
+
+for i in range(img_array.shape[0]):
+    for j in range(img_array.shape[1]):
+        pixels.append(
+            np.array((
+                j,
+                i,
+                img_array[i, j, 0],
+                img_array[i, j, 1],
+                img_array[i, j, 2]
+            ))
+        )
+pixels = np.array(pixels)
+print(pixels)
+
+print("start clustering")
+
+clusters = clustering(pixels, 256)
+
+print(clusters)
+
+triangles = build_triangles_delaunay(clusters[:, :2])
+
+print(triangles)
+
+img_draw = ImageDraw.Draw(img)
+
+for triangle in triangles:
+    img_draw.polygon(triangle.flatten().tolist(), outline=(0, 255, 0), width=1)
+
+img.show()
--- src/triangulate.py
+++ src/triangulate.py
@@ -1,11 +1,21 @@
 from PIL import Image
 from figure import Figure, Triangle
 from utils import *
+import numpy as np
+from sklearn.cluster import KMeans
+from skimage.filters import sobel
+from scipy.spatial import Delaunay
+from sklearn.preprocessing import MinMaxScaler
 
 
-def triangulate_from_file(path: str) -> list[list[Figure]]:
-    return triangulate(Image.open(path))
+# img - array of {"pixel_num": ("x", "y", "color1, color2, ...")}
+def clustering(img: np.ndarray, n_triangles: int) -> np.ndarray:
+    scaler = MinMaxScaler()
+    scaler.fit(img)
+    img_t = scaler.transform(img)
+    clusters = KMeans(n_clusters=n_triangles + 2).fit(img_t).cluster_centers_
+    return scaler.inverse_transform(clusters)
 
 
-def triangulate(gif: Image.Image) -> list[list[Figure]]:
-    raise NotImplementedError
+def build_triangles_delaunay(points: np.ndarray):
+    return points[Delaunay(points).simplices]
